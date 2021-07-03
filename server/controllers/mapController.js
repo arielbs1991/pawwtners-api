a829cb83b1e47b34a73ecb7d6b51d963eb692add
blugee <blugee>
2021-07-03 02:05:57 -0400

    changes for maps latitude and longitude

--- server/controllers/mapController.js
+++ server/controllers/mapController.js
@@ -4,11 +4,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 const authorize = require('../middlewares/authorize');
 
-router.get('/petShops', authorize(), async (req, res, next) => {
+router.get('/petShops?:latitude?:longitude', authorize(), async (req, res, next) => {
     try {
         let radius = req.userDetails.maximumDistance * 1609.34
         const data = await axios.get(
-            `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.userDetails.latitude},${req.userDetails.longitude}&radius=${radius}&types=pet_store&name=pet&key=${config.GOOGLE_MAP_TOKEN}`
+            `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.latitude},${req.query.longitude}&radius=${radius}&types=pet_store&name=pet&key=${config.GOOGLE_MAP_TOKEN}`
         )
 
         if (data.data.status === "OK") {
